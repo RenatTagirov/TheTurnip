bd12019ba7171bca71028f78e385a6bb8f5ef494
RenatTagitov <rena.tagirov@gmail.com>
2021-03-10 21:34:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> plot = new ArrayList<Person>();
+        plot.add(new Person("Репка", "Репку"));
+        plot.add(new Person("Дедка", "Дедку"));
+        plot.add(new Person("Бабка", "Бабку"));
+        plot.add(new Person("Внучка", "Внучку"));
+        TurnipStory.tell(plot);
+    }
+}
